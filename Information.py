bf7433f6b4e093cbdc34379dbe1215b098fb2da3
SuganthanAgshayan <134511197+SuganthanAgshayan@users.noreply.github.com>
2023-05-29 07:29:01 +0530

    Add files via upload

---
+++ Information.py
@@ -0,0 +1,5 @@
+FULL NAME        :    Suganthan Aghayan
+SCHOOL           :    KN/kilinochchi Maha vidyalayam
+GRADE            :    9(2022)
+ADDRESS          :    No22,paravippanchan,kilinochchi
+TELEPHONE NUMBER :    0773435807
