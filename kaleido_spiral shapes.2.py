35071f55fb27397e2e93184b4d871f5bc5d784ce
SuganthanAgshayan <134511197+SuganthanAgshayan@users.noreply.github.com>
2023-06-14 14:14:21 +0530

    Add files via upload

---
+++ kaleido_spiral shapes.2.py
@@ -0,0 +1,27 @@
+import turtle
+
+from itertools import cycle
+colors = cycle(['red','orange','yellow','green','blue','purple'])
+
+def draw_shape(size,angle,shift,shape):
+    turtle.pencolor(next(colors))
+    next_shape=''
+    if shape == 'circle':
+        turtle.circle(size)
+        next_shape= 'square'
+    elif shape =='square':
+        for i in range(6):
+            turtle.forward(size+80)
+            turtle.left(60)
+        next_shape = 'circle'
+    turtle.right(angle)
+    turtle.forward(shift)
+    draw_shape(size + 0,angle+0,shift+0,next_shape)
+
+
+
+turtle.bgcolor('black')
+turtle.speed('fastest')
+turtle.pensize(4)
+draw_shape(30,3,1, 'circle')
+
